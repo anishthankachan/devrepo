5d8407d437bbf9f9d771f78040bde33572820800
anishthankachan <45987067+anishthankachan@users.noreply.github.com>
2020-09-10 12:00:55 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+echo "Hello world!";
+#phpinfo();
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+use PhpOffice\PhpPresentation\PhpPresentation;
+use PhpOffice\PhpPresentation\IOFactory;
+use PhpOffice\PhpPresentation\Style\Color;
+use PhpOffice\PhpPresentation\Style\Alignment;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World !');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('/tmp/helloworld.xlsx');
+
+$objPHPPowerPoint = new PhpPresentation();
+
+// Create slide
+$currentSlide = $objPHPPowerPoint->getActiveSlide();
+
+// Create a shape (drawing)
+$shape = $currentSlide->createDrawingShape();
+$shape->setName('PHPPresentation logo')
+      ->setDescription('PHPPresentation logo')
+      ->setPath('./vendor/phpoffice/phppresentation/samples/resources/phppowerpoint_logo.gif')
+      ->setHeight(36)
+      ->setOffsetX(10)
+      ->setOffsetY(10);
+$shape->getShadow()->setVisible(true)
+                   ->setDirection(45)
+                   ->setDistance(10);
+
+// Create a shape (text)
+$shape = $currentSlide->createRichTextShape()
+      ->setHeight(300)
+      ->setWidth(600)
+      ->setOffsetX(170)
+      ->setOffsetY(180);
+$shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_CENTER );
+$textRun = $shape->createTextRun('Thank you for using PHPPresentation!');
+$textRun->getFont()->setBold(true)
+                   ->setSize(60)
+                   ->setColor( new Color( 'FFE06B20' ) );
+$oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+$oWriterPPTX->save("/tmp/sample.pptx");
+$oWriterODP = IOFactory::createWriter($objPHPPowerPoint, 'ODPresentation');
+$oWriterODP->save("/tmp/sample.odp");
+?>
